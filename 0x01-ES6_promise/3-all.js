1a909edb1d8bd3647c3ed5dc24de1c8608c12267
Njeri_ Maina <lucymaina4172@gmail.com>
2023-06-28 23:20:28 +0300

    lint fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -8,5 +8,5 @@ export default function handleProfileSignup() {
   return Promise.all([photo, user]).then((res) => {
     console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
   })
-  .catch(() => { console.log('Signup system offline'); });
+    .catch(() => { console.log('Signup system offline'); });
 }
