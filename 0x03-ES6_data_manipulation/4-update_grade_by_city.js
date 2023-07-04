05cff870b5eb815c7153aed648e0e8e6df5e8af4
Njeri_ Maina <lucymaina4172@gmail.com>
2023-07-04 20:02:43 +0300

    a function updateStudentGradeByCity that returns an array of students for a specific city with their new grade

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,12 @@
+export default function updateStudentGradeByCity(list, city, newGrades) {
+    return list
+      .filter((el) => el.location === city)
+      .map((student) => {
+        const grades = newGrades.filter((el) => el.studentId === person.id);
+        const grade = grades.length ? grades[0].grade : 'N/A';
+        return {
+          ...student,
+          grade,
+        };
+      });
+  }
