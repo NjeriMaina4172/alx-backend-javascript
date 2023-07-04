eece8f6a3103b3bc55be825e29be74d7280c1b8b
NjeriMaina4172 <lucymaina4172@gmail.com>
2023-07-04 17:07:59 +0000

    fixing lint

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,12 @@
 export default function updateStudentGradeByCity(list, city, newGrades) {
-    return list
-      .filter((el) => el.location === city)
-      .map((student) => {
-        const grades = newGrades.filter((el) => el.studentId === student.id);
-        const grade = grades.length ? grades[0].grade : 'N/A';
-        return {
-          ...student,
-          grade,
-        };
-      });
-  }
+  return list
+    .filter((el) => el.location === city)
+    .map((student) => {
+      const grades = newGrades.filter((el) => el.studentId === student.id);
+      const grade = grades.length ? grades[0].grade : 'N/A';
+      return {
+        ...student,
+        grade,
+      };
+    });
+}
